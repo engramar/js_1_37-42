40dc3568d11d758db4f17440d35251734e6c7944
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-14 22:00:38 +1100

    Create js_1_37-42.js

---
+++ js_1_37-42.js
@@ -0,0 +1,35 @@
+#Store Multiple Values in one Variable using JavaScript Arrays
+// Only change code below this line
+var myArray = ['one',1];
+
+#Nest one Array within Another Array
+// Only change code below this line
+var myArray = [["Bulldogs", 23], ["Eels", 45]]
+
+#Access Array Data with Indexes
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line
+var myData = myArray[0];
+
+#Modify Array Data With Indexes
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+#Access Multi-Dimensional Arrays With Indexes
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line
+var myData = myArray[2][1];
+
+#Manipulate Arrays With push()
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3])
